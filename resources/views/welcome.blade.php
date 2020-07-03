3cf0eeaa1e02c21209bc49a4726bc8ca2a71a723
unknown <daffaadkha@bitbucket.org>
2020-07-03 22:33:39 +0700

    Tambah Register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,6 +71,10 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
                     @endauth
                 </div>
             @endif
