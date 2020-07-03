d02b945d95950f225744f649af776244a7c41600
unknown <daffaadkha@bitbucket.org>
2020-07-03 22:39:25 +0700

    register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Poscandu
+                    Posyandu Bimomartani
                 </div>
             </div>
         </div>
